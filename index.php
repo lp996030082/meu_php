f3971a9bea87f19ccca6f18d96fb25e4f8fc0ef6
LETICIA ARAUJO PEREIRA <Leticia.1970@aluno.pr.senac.br>
2023-03-06 08:41:36 -0300

    php

---
+++ index.php
@@ -0,0 +1,21 @@
+<?php
+    $nome="leticia";
+    $idade=15;
+    $endereço="rua santa catarina, ap7, centro";
+    $cidade="ivaiporã";
+    $cep="86870000";
+    $primeirosemestre=10;
+    $segundosemestre=9;
+    $terceirosemestre=9;
+    $media=($primeirosemestre+$segundosemestre+$terceirosemestre)/3;
+        echo "seu nome é?" .$nome;
+        echo "sua idade é?" .$idade;
+        echo "seu endereço é?" .$endereço;
+        echo "sua cidade é?" .$cidade;
+        echo "seu cep é?" .$cep;
+        echo "sua nota no primeiro semestre é?" .$primeirosemestre;
+        echo "sua nota no segundo semestre é?" .$segundosemestre;
+        echo "sua nota no terceiro semestre é?" .$terceirosemestre;
+        echo "sua media é?" .$media;
+
+?>
